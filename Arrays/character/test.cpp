fdcbe1d61e303ebf3e0258af69e7a68ae1cc5d0d
Jugesh Kumar <jugeshsm@gmail.com>
2023-02-11 22:19:00 +0530

    char

---
+++ Arrays/character/test.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Jugesh Kumar on 11-02-2023.
+//
+#include <iostream>
+using namespace std;
+int main(){
+    char arr[100];
+//    //cin.getline(arr,100);
+//    // delimiter is like condition
+//    cin.getline(arr,100,'$');
+
+
+
+char ch;
+cin>>ch;
+int i=0;
+while(ch!='$'){
+    arr[i]=ch;
+    i++;
+    cin>>ch;
+}
+
+    cout<<arr<<endl;
+    return 0;
+}
