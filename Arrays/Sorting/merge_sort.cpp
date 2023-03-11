7ed3e1bcdcb9de80bbca6cb2088b5cb8cac290a6
Jugesh Kumar <jugeshsm@gmail.com>
2023-03-11 16:55:07 +0530

    ms

--- Arrays/Sorting/merge_sort.cpp
+++ Arrays/Sorting/merge_sort.cpp
@@ -1,5 +1,17 @@
 #include<iostream>
 using namespace std;
+void ms(int arr[],int start,int end){
+    //base case
+
+
+    //rec case
+    int mid=(start+(end-start))/2;
+    ms(arr,start,mid);
+
+}
 int main(){
+    int arr[]={3,1,2,7,6,8};
+    int n=sizeof(arr)/sizeof(int);
+    ms(arr,0,n-1);
     return 0;
 }
