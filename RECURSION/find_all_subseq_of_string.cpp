59c9d963b4fb0906b7e703ff1975dda780bf69e2
Jugesh Kumar <jugeshsm@gmail.com>
2023-03-12 11:37:37 +0530

    subsequence

--- RECURSION/find_all_subseq_of_string.cpp
+++ RECURSION/find_all_subseq_of_string.cpp
@@ -1,6 +1,25 @@
 #include<iostream>
 using namespace std;
+void subsequences(char inp[100],int i,char out[100],int j){
+    //base case
+    if(inp[i]=='\0'){
+        out[j]='\0';
+        cout<<out<<endl;
+        return;
+    }
+
+    //rec case
+    out[j]=inp[i];
+    subsequences(inp,i+1,out,j+1);
+
+    //without a
+    subsequences(inp,i+1,out,j);
+}
+
 int main(){
-    
-    reeturn 0;
+    char inp[100]="abc";
+    char out[100];
+    //cin>>inp; //"abc"
+    subsequences(inp,0,out,0);
+    return 0;
 }
