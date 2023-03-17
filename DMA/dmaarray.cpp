b584cad3d96e47305ac2c1f4b9c524a847e9e883
Jugesh Kumar <jugeshsm@gmail.com>
2023-03-17 19:53:08 +0530

    dma

---
+++ DMA/dmaarray.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int*aptr=new int[5];
+    for(int i=0;i<5;i++){
+        cin>>aptr[i];
+    }
+    for(int i=0;i<5;i++){
+        cout<<aptr[i]<<" ";
+    }
+    return 0;
+}
--- DMA/test.cpp
+++ DMA/test.cpp
@@ -4,11 +4,12 @@ int main(){
     //new = it will give us address
     int*aptr=new int; 
     *aptr=10;
-    cout<<*aptr;
+    cout<<*aptr<<endl;
 
     //delete keyword
     //delete a variable;
     delete aptr;
     aptr=NULL;
+    cout<<*aptr;
     return 0;
 }
