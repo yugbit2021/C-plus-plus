efe231095bbe51a65781e8726f804b2931569dd4
Jugesh Kumar <jugeshsm@gmail.com>
2023-03-19 17:00:18 +0530

    oops

--- OOPS/oops.cpp
+++ OOPS/oops.cpp
@@ -8,18 +8,25 @@ class car{
     string color;
     int price;
     int seats;
-    // car(){
-
-    // }
+    //constructor
+    car(string name,string color,int price,int seats){
+        cout<<"coding"<<endl;
+        this->name=name;
+        this->color=color;
+        this->price=price;
+        this->seats=seats;
+    }
 };
 
 int main(){
     //whenever object is created contructor is called 
-    car car1; //car1 is the object
-    car1.name="BMW";
-    car1.color="Black";
-    car1.price=1000000;
-    car1.seats=6;
+    // car car1; //car1 is the object
+    // car1.name="BMW";
+    // car1.color="Black";
+    // car1.price=1000000;
+    // car1.seats=6;
+
+    car car1("bmw","black",50,6);
     cout<<car1.name<<endl;
     cout<<car1.price<<endl;
 
