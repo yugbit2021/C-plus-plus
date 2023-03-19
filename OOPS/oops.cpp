ecd984e4f1b87d2fe391c385d352ef6829b6d24e
Jugesh Kumar <jugeshsm@gmail.com>
2023-03-19 16:42:12 +0530

    oops

---
+++ OOPS/oops.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class car{
+    public:
+
+    string name;
+    string color;
+    int price;
+    int seats;
+};
+
+int main(){
+    car car1;
+    car1.name="BMW";
+    car1.color="Black";
+    car1.price=1000000;
+    car1.seats=6;
+    cout<<car1.name<<endl;
+    cout<<car1.price<<endl;
+}
