98533492742daf9ffb9d8cc98bcca8bff1c25324
Jugesh Kumar <jugeshsm@gmail.com>
2023-04-09 12:56:08 +0530

    rec

--- RECURSION/Rat_in_a_maze.cpp
+++ RECURSION/Rat_in_a_maze.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
 
+
+
 int main(){
-    
+    char maze[][]={{'U','B','B','U'},{'U','B','B','U'},{'U','B','B','U'},{'U','B','B','U'}}
+    maze[i][j]=1;
+    //Forward
+   if(maze[i][j+1]!='B'){
+
+   }
+    //Backward
+   if(maze[i+1][j]!='B'){
+
+    }
     return 0;
 }
