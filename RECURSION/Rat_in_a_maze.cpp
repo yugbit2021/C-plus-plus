e471a441f350ae713211cf70c2173cd5467712d3
Jugesh Kumar <jugeshsm@gmail.com>
2023-04-09 14:08:56 +0530

    maze rec

--- RECURSION/Rat_in_a_maze.cpp
+++ RECURSION/Rat_in_a_maze.cpp
@@ -1,15 +1,26 @@
 #include<iostream>
 using namespace std;
 
-bool ratinmaze(char maze[4][4],sol[4][4],int i, int j,int rows,int cols){
+bool ratinmaze(char maze[4][5],int sol[4][4],int i, int j,int rows,int cols){
     //base case
+    if(i==rows-1 && j==cols-1){
+        sol[i][j]=1;
+        //print sol matrix
+        for(int l=0;l<rows;l++){
+            for(int m=0;m<cols;m++){
+                cout<<sol[l][m]<<" ";
+            }
+            cout<<endl;
+        }
+        return true;
+    }
 
     //recursive case
 
     sol[i][j]=1;
 
     //Forward
-   if(j<=cols-1 &&maze[i][j+1]!='B'){
+   if(j<=cols-2 &&maze[i][j+1]!='B'){
     bool aagekaansmila=ratinmaze(maze,sol,i,j+1,rows,cols);
     if(aagekaansmila==true){
         return true;
@@ -17,7 +28,7 @@ bool ratinmaze(char maze[4][4],sol[4][4],int i, int j,int rows,int cols){
    }
 
    //Backward
-   if(i<=rows-1&&maze[i+1][j]!='B'){
+   if(i<=rows-2 &&maze[i+1][j]!='B'){
     bool aagekaansmila=ratinmaze(maze,sol,i+1,j,rows,cols);
     if(aagekaansmila==true){
         return true;
@@ -29,12 +40,9 @@ bool ratinmaze(char maze[4][4],sol[4][4],int i, int j,int rows,int cols){
 }
 
 int main(){
-    char maze[4][4]={{'U','B','B','B'},
-                   {'U','B','U','U'},
-                   {'U','B','B','U'},
-                   {'U','U','U','U'}};
+    char maze[4][5]={"UBUU","UUUU","BBBU","UBBU"}; // null char h isliye 5 * 5 liye
 
     int sol[4][4]={0};
-    ratinmaze(maze,sol,0,0);
+    ratinmaze(maze,sol,0,0,4,4);
     return 0;
 }
