f24cec631a50942dacbbeabf8a451e17316ba6a1
Jugesh Kumar <jugeshsm@gmail.com>
2023-04-10 16:18:42 +0530

    backtrack

---
+++ RECURSION/backtrack/Phonekeyboard.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+string arr[]={"","","ABC","DEF","GHI","JKL","MNO","PQRS","TUV","WXYZ"};
+
+void phonekeypad(string inp,int i,string out){
+    if(inp[i]=='\0'){
+        cout<<out<<endl;
+        return;
+    }
+
+
+    char ch=inp[i];//"2"
+    int indx=ch-'0';//'2'-'0'=2
+
+    for(int l=0;arr[indx][l]!='\0';l++){
+        //out[j]=arr[indx][l];
+        out=out+arr[indx][l];
+        phonekeypad(inp,i+1,out);
+    }
+    
+}
+
+int main(){
+    //string inp;
+    string inp;
+    string out="";
+    cin>>inp; // "23"
+
+    phonekeypad(inp,0,out);
+    return 0;
+}
