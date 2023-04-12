731a306529a7f1ee7e7efb320f115f44a020f970
Jugesh Kumar <jugeshsm@gmail.com>
2023-04-12 10:08:01 +0530

    doublyll

---
+++ LinkedList/doublyll.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+ class node{
+    public:
+
+    node*prev;
+    int data;
+    node*next;
+    node(int d){
+        prev=NULL;
+        data=d;
+        next=NULL;
+    }
+};
+int main(){
+
+    return 0;
+}
