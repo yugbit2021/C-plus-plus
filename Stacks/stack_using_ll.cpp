b150bfe1a249272db9135ac3b107c86f20d82b25
Jugesh Kumar <jugeshsm@gmail.com>
2023-04-15 07:51:33 +0530

    stack susing ll

---
+++ Stacks/stack_using_ll.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+//template<typename U>
+class node{
+    public: 
+
+    int data;
+    node*next;
+    node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+
+class stack{
+    node*head;
+    int l;
+    
+    public:
+    stack(){
+        head=NULL;
+        l=0;
+    }
+
+    void push(char d){
+        node*n=new node(d);
+        n->next=head;
+        head=n;
+        l++;
+    }
+
+
+    void pop(){
+       //if ll is empty
+    if (head==NULL){
+        return;
+    }
+    // ll is only one node
+    else if(head->next==NULL){
+        delete head;
+        head=NULL;
+    }
+    else{
+        node*temp=head->next;
+        delete head;
+        head=temp;
+    }
+    l--;
+    }
+
+
+    char size(){
+        return l;
+    }
+
+
+    char top(){
+       return head->data;
+    }
+
+
+    bool empty(){
+        return head==NULL;
+    }
+};
+
+int main(){
+    stack s;
+    s.push('A');
+    s.push('B');
+    s.push('C');
+
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+    return 0;
+}
