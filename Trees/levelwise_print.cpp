cca7651ab81ad9b77e596831f8a1354d4f523db8
Jugesh Kumar <jugeshsm@gmail.com>
2023-04-18 05:25:51 +0530

    .

---
+++ Trees/levelwise_print.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class node{
+    public:
+    int data;
+    node*left;
+    node*right;
+
+    node(int d){
+        data=d;
+        left=NULL;
+        right=NULL;
+    }
+};
+node* buildtress(){
+    int d;
+    cin>>d;
+    if(d==-1){
+        return NULL;
+    }
+    else{
+    node*root=new node(d);
+
+    root->left=buildtress();
+    root->right=buildtress();
+    return root;
+}
+}
+
+void levelwise(node*root){
+	queue<node*> q;
+	q.push(root);//8 NULL
+	q.push(NULL);
+
+
+// loop
+	while(!q.empty()){
+		node*x=q.front();//NULL
+	q.pop();
+	if(x==NULL){
+		cout<<endl;
+		if(!q.empty()){
+			q.push(NULL);
+
+		}
+		
+
+	}
+	else{
+		cout<<x->data<<" ";//8
+
+	if(x->left!=NULL){
+		q.push(x->left);
+	}
+
+	if(x->right!=NULL){
+		q.push(x->right);
+	}
+
+	}
+	
+
+
+	}
+	
+
+
+
+
+}
+
+int main(){
+   node*root=buildtress(); 
+   levelwise(root);  
+   return 0;
+}
