5f699159300f655e00a78d1141809d7b0d064b81
Jugesh Kumar <jugeshsm@gmail.com>
2023-04-17 16:36:24 +0530

    .

--- Trees/tress.cpp
+++ Trees/tress.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 class node{
     public:
-
     int data;
     node*left;
     node*right;
@@ -81,10 +80,8 @@ int sumofnode(node*root){
     if(root==NULL){
         return 0;
     }
-    
-
     //rec case
-    return sumofnode(root->left)+countnode(root->right)+root->data;
+    return sumofnode(root->left)+sumofnode(root->right)+root->data;
 }
 bool searchin_a_bt_tree(node*root,int key){
     //base case
@@ -93,15 +90,23 @@ bool searchin_a_bt_tree(node*root,int key){
     }
     
     //rec case
-    if(root->data==key || searchin_a_bt_tree(root-left,key) || searchin_a_bt_tree(root->right,key)){
+    if(root->data==key || searchin_a_bt_tree(root->left,key) || searchin_a_bt_tree(root->right,key)){
         return true;
     }
     return false;
 }
 
+int heightoftree(node*root){
+    //base case
+    if(root==NULL){
+        return 0;
+    }
+
+    //rec case
+    return max(heightoftree(root->left),heightoftree(root->right))+1;
+}
 
 int main(){
-   
    node*root=buildtress();
    cout<<"pre order is ";
    preorder(root);
@@ -112,9 +117,12 @@ int main(){
    cout<<"post order is ";
    postorder(root);
    cout<<endl;
-   cout<<" count of nodes are "<<countnode(root)<<endl;
-   cout<<" sum of nodes are "<<sumofnode(root)<<endl;
-   if(searchin_a_bt_tree(root,13)==true){
+   cout<<"count of nodes are "<<countnode(root)<<endl;
+   cout<<"sum of nodes are "<<sumofnode(root)<<endl;
+   cout<<"height of tree "<<heightoftree(root)<<endl;
+   int key;
+   cin>>key;
+   if(searchin_a_bt_tree(root,key)==true){
     cout<<"key is present "<<endl;
    }
    
