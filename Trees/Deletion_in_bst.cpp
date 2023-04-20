b78d13354c0cf403d2d0d69aed4e9c5f56f26645
Jugesh Kumar <jugeshsm@gmail.com>
2023-04-20 21:54:51 +0530

    deletion in bst

--- Trees/Deletion_in_bst.cpp
+++ Trees/Deletion_in_bst.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
 class node{
@@ -38,36 +39,114 @@ node*buildbst(){
     }
     return root;
 }
-bool checkbst(node*root,int min=INT_MIN,int max=INT_MAX){
+
+node* deletioninbst(node*root,int key){
 	// base case
 	if(root==NULL){
-		return true;
+		return NULL;
+
 	}
+
 
 	// rec case
-	if(root->data>=min and root->data<=max and checkbst(root->left,min,root->data) and checkbst(root->right,root->data+1,max)){
-		return true;
+	// key lie in lst
+	if(key<root->data){
+		root->left=deletioninbst(root->left,key);
+		return root;
 	}
-	return false;
+	// key lie rst
+	else if(key>root->data){
+		root->right=deletioninbst(root->right,key);
+		return root;
+	}
 
+	// key is at root node 
+	else{
+		// root ka lst and rst doesn;t exist
+		if(root->left==NULL and root->right==NULL){
+			delete root;
+			root=NULL;
+			return root;
+		}
+		// case 2:  root ka lst exist and rst doesn;t exist
+		else if(root->left!=NULL and root->right==NULL){
+			node*temp=root->left;
+			delete root;
+			root=NULL;
+			return temp;
+		}
+
+		// case 3:  root ka lst exist and rst doesn;t exist
+		else if(root->left==NULL and root->right!=NULL){
+			node*temp=root->right;
+			delete root;
+			root=NULL;
+			return temp;
+
+		}
+		else{
+			// nboth lst and rst exist
+			// // i am choosing max of lst as root
+			node*temp=root->left;
+
+			while(temp->right!=NULL){
+				temp=temp->right;
+
+			}
+			swap(root->data,temp->data);
+			// delete temp;
+			root->left=deletioninbst(root->left,key);
+			return root;
+
+			// i am choosing min of rst as root
+			// node*temp=root->right;
+
+			// while(temp->left!=NULL){
+			// 	temp=temp->left;
+
+			// }
+			// swap(root->data,temp->data);
+			// // delete temp;
+			// root->right=deletioninbst(root->right,key);
+			// return root;
+
+
+
+		}
+	}
 }
-deletioninbst(node*root,int key){
-    //base case
-    //rec case
-    //key lie in lst
-    if(key<root->data){
+void levelwise(node*root){
+	queue<node*> q;
+	q.push(root);//8 NULL
+	q.push(NULL);
 
-    }
-    //key lie in rst
-    else if(key>root->data){
 
-    }
-    //key is at node
-    else{
-        
-    }
+// loop
+	while(!q.empty()){
+		node*x=q.front();//NULL
+	q.pop();
+	if(x==NULL){
+		cout<<endl;
+		if(!q.empty()){
+			q.push(NULL);
+		}
+	}
+	else{
+		cout<<x->data<<" ";//8
+
+	if(x->left!=NULL){
+		q.push(x->left);
+	}
+
+	if(x->right!=NULL){
+		q.push(x->right);
+	}
+	}
+	}
 }
 int main(){
     node*root=buildbst();
+    deletioninbst(root,8);
+	levelwise(root);
 
 }
