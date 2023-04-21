feae37a7e0916b275a13531d7837d4a7a128bf01
Jugesh Kumar <jugeshsm@gmail.com>
2023-04-21 05:48:25 +0530

    Search in roteted sorted array

--- Practice sessions/search_in_sorted_array.cpp
+++ Practice sessions/search_in_sorted_array.cpp
@@ -1,19 +1,77 @@
 //https://leetcode.com/problems/search-in-rotated-sorted-array/
-class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        int lo=0;
-        int hi=nums.end();
-        
+#include<iostream>
+using namespace std;
 
-        while(lo<hi){
-            int mid=(hi+lo)/2;
-            if(nums[mid]>target &&){
-                lo=mid+1;
+//     class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         int lo=0;
+//     int hi=nums.size()-1;
+
+//     int ans=-1;
+
+//     while(lo<=hi){
+//         int mid=(lo+hi)/2;
+
+//         if(nums[mid]==target){
+//             ans=mid;
+//             break;
+//         }
+//         if(nums[lo]<=nums[mid]){
+//             if(nums[mid]>=target && nums[lo]<=target){
+//                 hi=mid-1;
+//             }
+//         else{
+//             lo=mid+1;
+//         }}
+//         else{
+//             if(nums[mid]<=target && target<=nums[hi]){
+//                 lo=mid+1;
+//             }else{
+//                 hi=mid-1;
+//             }
+//         }
+//     }
+//     return ans;
+//     }
+// };
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cin>>target;
+
+    int lo=0;
+    int hi=n-1;
+
+    int ans=-1;
+
+    while(lo<=hi){
+        int mid=(lo+hi)/2;
+
+        if(arr[mid]==target){
+            ans=mid;
+            break;
+        }
+        if(arr[lo]<=arr[mid]){
+            if(arr[mid]>=target && arr[lo]<=target){
+                hi=mid-1;
             }
-            else if(nums[mid]<target){
-
+        else{
+            lo=mid+1;
+        }}
+        else{
+            if(arr[mid]<=target && target<=arr[hi]){
+                lo=mid+1;
+            }else{
+                hi=mid-1;
             }
         }
     }
-};
+    cout<<ans;
+}
