2325af810e3c57be76a7c6bcd9aa75f95ddc0526
Jugesh Kumar <jugeshsm@gmail.com>
2023-04-23 09:33:03 +0530

    ..

---
+++ Practice sessions/Longest_Common_prefix.cpp
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/longest-common-prefix/submissions/937687641/
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(),strs.end());
+        int a=strs.size();
+        string n=strs[0],m=strs[a-1],ans="";
+        for(int i=0;i<n.size();i++){
+            if(n[i]==m[i]){ans+=n[i];}
+            else break;
+        }
+        return ans;
+        
+    }
+};
