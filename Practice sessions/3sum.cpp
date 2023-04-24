2e9981ad1193b61ebfd9ebe94c89b88266aff9c2
Jugesh Kumar <jugeshsm@gmail.com>
2023-04-24 15:18:06 +0530

    ..

--- Practice sessions/3sum.cpp
+++ Practice sessions/3sum.cpp
@@ -1,30 +1,53 @@
-class Solution {
-public:
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<int> ans;
-        for(int i=0;i<nums.size()-1;i++){
-            int first=nums[i];
-            int temp=0-first;
+        int target=0;
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        for(int i=0;i<nums.size();i++){
+            
             int low=i+1;
         int high= nums.size()-1;
         vector<int> ans1;
         while(low<high){
-            int sum=nums[low]+nums[high];
-            if(sum==temp){
-               ans1.push_back(temp);
-               ans1.push_back(low);
-               ans1.push_back(high);
+            int sum=nums[low]+nums[high]+nums[i];
+            if(sum==target){
+               ans1.push_back(nums[i]);
+               ans1.push_back(nums[low]);
+               ans1.push_back(nums[high]);
+               low++;
+               high--;
             }
-            if(sum>temp){
+            if(sum>target){
                 high--;
             }
-            else if(sum<temp){
+            else if(sum<target){
                 low++;
-            }
+       }
         }
         ans.push_back(ans1);
 
     }
     return ans;
         }
-};
+
+int main(){
+
+    vector<int> x;
+    x.push_back(-1);
+    x.push_back(0);
+    x.push_back(1);
+    x.push_back(2);
+    x.push_back(-1);
+    x.push_back(4);
+
+    vector<vector<int>> ans= threeSum(x);
+
+    for(int i=0;i<ans.size();i++){
+        for(int j=0;j<ans[i].size();j++){
+            cout<<ans[i][j]<<" ";
+        }
+    }
+}        
