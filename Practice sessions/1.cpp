119c0373f668cdcd93a2bb22108a67567d00a042
Jugesh Kumar <jugeshsm@gmail.com>
2023-05-04 19:10:08 +0530

    ..

---
+++ Practice sessions/1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+
+    while(t--){
+        long long int n;
+        int k;
+        cin>>n>>k;
+        if(k==1){
+            cout<<n<<endl;
+            continue;
+        }
+    }
+}
