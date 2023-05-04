4a4621b9aeb51169902c94b04a4f2115e2af8ea9
Jugesh Kumar <jugeshsm@gmail.com>
2023-05-04 20:20:46 +0530

    char

---
+++ Char/test.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //how to declare char arrays
+    //datatype nameofarr[size];
+    // char arr[]={'a','b','c','\0'};
+    // // for(int i=0;arr[i]!='\0';i++){
+    // //     cout<<arr[i]<<" ";
+    // // }
+    // cout<<arr<<endl;
+
+    char arr[]="dog";
+    //by default last me \0 daal hua hota hai
+    cout<<arr<<endl;
+    return 0;
+}
