bd5639ec91c552ba70d60de7460848dc44b2391c
Jugesh Kumar <jugeshsm@gmail.com>
2023-05-05 09:24:29 +0530

    Strings

---
+++ Strings/test.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // string is a char array terminated by null
+    // string s="hello";
+
+//     string s;
+//     // cin>>s;//hello world ---> hello (space ke baad input nahi lega)
+//    // getline(cin,stringname);
+//    getline(cin,s);
+
+string s1="pen";
+string s2="dog";
+
+//If I want to append + 
+cout<<s1+s2<<endl;
+
+    
+    return 0;
+}
