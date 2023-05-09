81e6b6e6f91836d7fdd6b1e60d2e1ece6218c8e8
Jugesh Kumar <jugeshsm@gmail.com>
2023-05-09 09:39:18 +0530

    spiral print

---
+++ Loops/spiralprint.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+void spiral(int arr[3][3],int tr,int tc){
+    int startrow=0;
+    int startcol=0;
+    int endcol=tc-1;
+    int endrow=tr-1;
+
+    while(startcol<=endcol && startrow<=endcol){
+
+    
+//pehla row ka work done
+    for(int i=startcol;i<=endcol;i++){
+    cout<<arr[startrow][i]<<" ";
+    }
+    startrow++;//1
+    //last col;
+    for(int j=startrow;j<=endrow;j++){
+        cout<<arr[j][endcol]<<" ";
+    }
+    endcol--;
+    for(int k=endcol;k>=startcol;k--){
+        cout<<arr[endrow][k]<<" ";
+    }
+    endrow--;
+    for(int l=endrow;l>=startrow;l--){
+        cout<<arr[l][startcol]<<" ";
+    }
+    startcol++;
+
+}
+}
+
+int main(){
+    int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    spiral(arr,3,3);
+}
