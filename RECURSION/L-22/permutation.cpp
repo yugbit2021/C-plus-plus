e4781dab6c98883920a91dfec9b1de763c8a3611
Jugesh Kumar <jugeshsm@gmail.com>
2023-05-20 15:41:42 +0530

    permutation

--- RECURSION/L-22/permutation.cpp
+++ RECURSION/L-22/permutation.cpp
@@ -2,16 +2,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int co=0;
 void permutation(char inp[1000],int i){
     //base case
     if(inp[i]=='\0'){
         cout<<inp<<endl;
+        co++;
         return;
     }
     //rec case
     for(int j=i;inp[j]!='\0';j++){
            swap(inp[i],inp[j]);//swap
            permutation(inp,i+1); //abc acb
+           swap(inp[i],inp[j]);//backtracking
 }
 }
 int main(){
@@ -19,4 +22,6 @@ int main(){
     cin>>inp;
     //char out[1000];
     permutation(inp,0);
+
+    cout<<co;
 }
