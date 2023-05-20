7fc69b5b857287ffc1ce27387b85070c7b75dbfa
Jugesh Kumar <jugeshsm@gmail.com>
2023-05-20 16:46:46 +0530

    nstairs

--- RECURSION/L-22/nstairs.cpp
+++ RECURSION/L-22/nstairs.cpp
@@ -1,10 +1,19 @@
 #include<iostream>
 using namespace std;
 
-void nstairs(){
+//maximum he can jump 3 steps
+int nstairs(int n){
     //base case
+    if(n<0){
+        return 0;
+    }
+    if(n==0){
+        return 1;
+    }
     //rec case
+    return nstairs(n-1)+nstairs(n-2)+nstairs(n-3);
 }
 
 int main(){
+    cout<<nstairs(5);
 }
