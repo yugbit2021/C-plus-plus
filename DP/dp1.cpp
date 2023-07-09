065e887d041f2a89a8036789b0804c4f8d07848d
Jugesh Kumar <jugeshsm@gmail.com>
2023-07-09 06:47:50 +0530

    dp fibo

--- DP/dp1.cpp
+++ DP/dp1.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+
 // int fibo(int n){
 //     if(n==0 || n==1){
 //         return n;
@@ -7,41 +8,35 @@ using namespace std;
 //     return fibo(n-1)+fibo(n-2);
 // }
 
-//topdown sol
-int dpfibo(int n,int *arr){
+//topdown solution
+int dpfibo(int n,int*arr){
     if(n==0 || n==1){
         return arr[n]=n;
     }
-    //calculate se pahle check
+    //Check before calculate
     if(arr[n]!=-1){
         return arr[n];
     }
+    // store before return
     return arr[n]=dpfibo(n-1,arr)+dpfibo(n-2,arr);
 }
 
-//bottom up solution
+//bottomup solution
 int fibobottomup(int n){
     int *arr=new int[n+1];
     arr[0]=0;
     arr[1]=1;
 
-    for(int i=2;i<=n;i++){
-         arr[i]=arr[i-1]+arr[i-2];
+    for(int i=2;i<n+1;i++){
+        arr[i]=arr[i-1]+arr[i-2];
     }
-    // for(int i=0;i<=n;i++){
-    //     cout<<arr[i]<<" ";
-    // }
-    //cout<<endl;
-   return arr[n];
-    
-    }
+    return arr[n];
+}
 
 int main(){
     int n;
     cin>>n;
-    //cout<<fibo(n)<<endl;
-
-    //
+    
 int *arr=new int[n+1];
 for(int i=0;i<=n;++i){
     arr[i]=-1;
