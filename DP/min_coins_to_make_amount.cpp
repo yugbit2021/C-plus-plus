49a62ebf06009d1629dc163e7a4d13b601e4c344
Jugesh Kumar <jugeshsm@gmail.com>
2023-07-17 07:32:51 +0530

    min coins to need amount

--- DP/min_coins_to_make_amount.cpp
+++ DP/min_coins_to_make_amount.cpp
@@ -3,45 +3,46 @@
 using namespace std;
 
 int mincoinsneeded(int deno[],int amount,int n){
-
+    //base case
     if(amount==0){
         return 0;
     }
+
     //rec case
     int ans=INT_MAX;
     for(int j=0;j<n;j++){
-    int bachaamount=amount-deno[j];
-    if(bachaamount>=0){
-            int a=mincoinsneeded(deno,bachaamount,n);
-           ans=min(ans,a+1);
-           
+    int remaining_amount=amount-deno[j];//12
+    if(remaining_amount>=0){
+        int a=mincoinsneeded(deno,remaining_amount,n);//4
+        ans=min(ans,a+1);
     }
-}
-return ans;
+    }
+    return ans;
 }
 
+//topdown
 int tdmincoinsneeded(int deno[],int amount,int n,int *arr){
-
+    //base case
     if(amount==0){
         return arr[amount]=0;
     }
+
     //rec case
     if(arr[amount]!=INT_MAX){
         return arr[amount];
     }
     int ans=INT_MAX;
     for(int j=0;j<n;j++){
-    int bachaamount=amount-deno[j];
-    if(bachaamount>=0){
-            int a=tdmincoinsneeded(deno,bachaamount,n,arr);
-           ans=min(ans,a+1);
-           
+    int remaining_amount=amount-deno[j];//12
+    if(remaining_amount>=0){
+        int a=tdmincoinsneeded(deno,remaining_amount,n,arr);//4
+        ans=min(ans,a+1);
     }
-}
-return arr[amount]=ans;
+    }
+    return arr[amount]=ans;
 }
 
-
+//bottomup
 int bumincoinsneeded(int deno[],int amount,int n){
 
    int *arr=new int[amount+1];
@@ -60,20 +61,20 @@ int bumincoinsneeded(int deno[],int amount,int n){
    return arr[amount];
 }
 
-
 int main(){
     int deno[]={1,5,6,9};
     int n=sizeof(deno)/sizeof(int);
-    int amount;cin>>amount;
- //cout<< mincoinsneeded(deno,amount,n);
-int arr[amount+1];
-for(int i=0;i<amount+1;i++){
-arr[i]=INT_MAX;
-}
-cout<<bumincoinsneeded(deno,amount,n)<<endl;
-cout<<tdmincoinsneeded(deno,amount,n,arr)<<endl;
+    int amount;
+    cin>>amount;
+    
+    int arr[amount+1];
+    for(int i=0;i<amount+1;i++){
+        arr[i]=INT_MAX;
+    }
 
-cout<< mincoinsneeded(deno,amount,n);
+    cout<<bumincoinsneeded(deno,amount,n)<<endl;
+    
+    cout<<tdmincoinsneeded(deno,amount,n,arr)<<endl;
 
-    return 0;
+    cout<<mincoinsneeded(deno,amount,n)<<endl;
 }
