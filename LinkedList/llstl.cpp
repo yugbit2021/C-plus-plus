b8caa3d02667c353ada0c28e532f4a0e3cd7e8ec
Jugesh Kumar <jugeshsm@gmail.com>
2023-07-24 06:11:04 +0530

    linkedlist stl

--- LinkedList/llstl.cpp
+++ LinkedList/llstl.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 int main(){
     list<int> l;
+
     l.push_back(7);
-    l.push_back(5);
-    l.push_back(7);
-    l.push_back(5);
+    l.push_back(9);
+    l.push_back(1);
+    l.push_back(6);
+    l.push_back(3);
 
     for(auto x:l){
         cout<<x<<" ";
